2c1d4897e8cdad93f1e47fe220a4de5559900a2a
FunNoober <71403430+FunNoober@users.noreply.github.com>
2021-06-27 11:32:56 -0400

    Routine Backup

--- Single Server Assets Uncommpressed/Scripts/Player/PlayerMovement.cs
+++ Single Server Assets Uncommpressed/Scripts/Player/PlayerMovement.cs
@@ -2,43 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 8f;
-    public float runSpeed = 11f;
-
+    [Tooltip("The Player Controller")]
+    public CharacterController playerController;
+    [Tooltip("The Child Cam of this Object")]
+    public Camera mainCam;
+    [Tooltip("Move Speed With No Run")]
+    public float moveSpeed = 5;
+    [Tooltip("Movement Speed While Running")]
+    public float runSpeed = 8;
+    [Tooltip("The Max Amount of Staminia")]
+    public float maxStamina = 10;
+    [Tooltip("The Remaining Staminia")]
+    public float currentStamina = 10;
+    [Tooltip("The Sensitivity of The Mouse")]
+    public float mouseSensitivity = 500;
+    [Tooltip("The Gravity Force")]
     public float gravity = -9.81f;
-    public float groundDistance = 0.5f;
-    public float stamina = 10f;
-    public float maxStamina = 10f;
+    [Tooltip("How Small The Player Should Get While Crouching")]
+    public float crouchHeight;
 
-    public float defaultMoveSpeed;
-    public float defaultRunSpeed;
-
-    Vector3 velocity;
-    public Transform groundCheck;
-
-    public CharacterController charController;
+    [Tooltip("The Size of The Sphere when Checking for Ground")]
+    public float groundDistance = 0.1f;
+    [Tooltip("The Force of The Jump")]
+    public float jumpHeight = 3;
+    [Tooltip("The Object Where To Check for Ground")]
+    public GameObject groundCheck;
+    [Tooltip("The Objects That Are Ground")]
     public LayerMask groundMask;
 
-    bool isGrounded;
-    public bool isRunning;
-    public bool canRun = true;
+    [Header("Should Use")]
+    public bool shouldUseStaminia = true;
+    public bool hasRun = true;
+    public bool hasGravity = true;
+    public bool canCrouch = true;
+    public bool shouldUseJump = true;
+    //public bool hasAirStrafe = true;
+    [Header("Key Binds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode runKey = KeyCode.LeftShift;
 
-    public float jumpHeight = 10;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private bool canRun = true;
+    private float xRotation;
+    private float startPlayerHeight;
 
-    public bool isController = false;
+    private float xVector;
+    private float zVector;
 
     public KeyboardActions keyboardControls;
 
-    float xVector;
-    float zVector;
     void Awake()
     {
-        defaultMoveSpeed = moveSpeed;
-        defaultRunSpeed = runSpeed;
-
         keyboardControls = new KeyboardActions();
+        playerController = GetComponent<CharacterController>();
+        if (shouldUseStaminia == true)
+            currentStamina = maxStamina;
+
+        startPlayerHeight = playerController.height;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void OnEnable()
@@ -51,60 +82,67 @@ public class PlayerMovement : MonoBehaviour
         keyboardControls.Disable();
     }
 
+
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -3f;
         }
+
         xVector = keyboardControls.Controls.Movement.ReadValue<Vector2>().x;
         zVector = keyboardControls.Controls.Movement.ReadValue<Vector2>().y;
-        
+
         Vector3 move = transform.right * xVector + transform.forward * zVector;
-        charController.Move(move * moveSpeed * Time.deltaTime);
+        playerController.Move(move * moveSpeed * Time.deltaTime);
 
-
-        if (Keyboard.current.leftShiftKey.isPressed && canRun == true)
+        if (Keyboard.current.leftShiftKey.isPressed && canRun == true && hasRun == true)
         {
-            charController.Move(move * runSpeed * Time.deltaTime);
-            stamina -= Time.deltaTime;
+            playerController.Move(move * runSpeed * Time.deltaTime);
+            if (shouldUseStaminia == true)
+                currentStamina -= Time.deltaTime;
         }
 
-        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && shouldUseJump)
             Jump();
 
-        if (stamina <= 0)
+        if (Keyboard.current.ctrlKey.isPressed && canCrouch)
         {
-            canRun = false;
-            StartCoroutine(GiveStamina());
+            playerController.height = crouchHeight;
         }
 
-        if (stamina >= maxStamina)
+        else
+            playerController.height = startPlayerHeight;
+
+        if (currentStamina <= 0)
+        {
+            canRun = false;
+            StartCoroutine(GiveStaminia());
+        }
+
+        if (currentStamina > 0)
         {
             canRun = true;
         }
 
-        stamina = Mathf.Clamp(stamina, 0, maxStamina - 1);
+        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
 
-        velocity.y += gravity * Time.deltaTime;
-        charController.Move(velocity * Time.deltaTime);
+        if (hasGravity == true)
+            velocity.y += gravity * Time.deltaTime;
+
+        playerController.Move(velocity * Time.deltaTime);
+
+        float mouseX = keyboardControls.Controls.Turning.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
+        float mouseY = keyboardControls.Controls.Turning.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -85f, 65f);
+
+        mainCam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
     }
 
-    void LateUpdate()
-    {
-        if (Keyboard.current.leftCtrlKey.isPressed)
-        {
-            charController.height = 0.5f;
-        }
-
-        else
-        {
-            charController.height = 2f;
-            moveSpeed = defaultMoveSpeed;
-            runSpeed = defaultRunSpeed;
-        }
-    }
 
     void Jump()
     {
@@ -112,14 +150,13 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = jumpHeight;
             velocity.y -= gravity * Time.deltaTime;
-            charController.Move(velocity * Time.deltaTime);
+            playerController.Move(velocity * Time.deltaTime);
         }
     }
 
-    IEnumerator GiveStamina()
+    IEnumerator GiveStaminia()
     {
-        yield return new WaitForSeconds(4);
-        stamina += 1f;
+        yield return new WaitForSeconds(3);
+        currentStamina += Time.deltaTime;
     }
-
 }
